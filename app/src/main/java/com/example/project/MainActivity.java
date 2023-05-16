65434e769e363911a952b63302a961bb58ff4593
c22mohel <c22mohel@student.his.se>
2023-05-16 13:56:09 +0200

    skapar en prefrence object

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
     Button click;
 
+
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +25,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // används för att kunna lagra preference
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        // används för att kunna edit preference
+        myPreferenceEditor = myPreferenceRef.edit();
+
         click = findViewById(R.id.swap);
+
+        //skapar en ny instans av textview
+        TextView prefTextRef=new TextView(this);
+        //länkar preftextref med id
+        prefTextRef=(TextView)findViewById(R.id.textView123);
+        //
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
 
         click.setOnClickListener(new View.OnClickListener() {
             @Override
